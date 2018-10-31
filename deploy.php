9e0a2d54d297359c005eef600281a20e370b6abf
WJ <weijianl-wa14@student.tarc.edu.my>
2018-10-31 17:27:20 +0800

    CI TEST

---
+++ deploy.php
@@ -0,0 +1,17 @@
+<?php
+namespace Deployer;
+
+include_once __DIR__ . '/vendor/autoload.php';
+include_once __DIR__ . '/vendor/deployer/deployer/recipe/composer.php';
+
+host('172.104.170.27')
+	->port(22)
+	->set('deploy_path', '/opt/lampp/htdocs/jian')
+	->user('root')
+	->set('branch', 'master')
+	->stage('production')
+	->identityFile('~/.ssh/id_rsa');
+
+set('repository', 'git@github.com:inotsucker/bank.git');
+
+set('keep_releases', 5);
